2c920ad64f68612c91ee0a71da20c77fee58aa79
4oliow <162386086+4oliow@users.noreply.github.com>
2025-04-05 12:43:57 +0200

    Add files via upload
    Added a fastboot button, changed colors and shapes of the buttons

--- MainActivity.cs
+++ MainActivity.cs
@@ -8,21 +8,37 @@ using AndroidX.AppCompat.App;
 using Google.Android.Material.FloatingActionButton;
 using Google.Android.Material.Snackbar;
 using Android.Widget;
+using Android.Graphics.Drawables;
 
 namespace RecoveryTestApp
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)] 
     public class MainActivity : Activity
     {
+        
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            Button rebootButton = FindViewById<Button>(Resource.Id.rebootButton);
+            Button fastbootButton = FindViewById<Button>(Resource.Id.fastbootButton);
+            Button recoveryButton = FindViewById<Button>(Resource.Id.recoveryButton);
             TextView statusText = FindViewById<TextView>(Resource.Id.statusText);
 
-            rebootButton.Click += (sender, e) =>
+
+            var roundRecovery = new Android.Graphics.Drawables.GradientDrawable();
+            roundRecovery.SetCornerRadius(32f);
+            roundRecovery.SetColor(Android.Graphics.Color.DarkRed);
+            recoveryButton.Background = roundRecovery;
+
+            var roundFastboot = new Android.Graphics.Drawables.GradientDrawable();
+            roundFastboot.SetCornerRadius(32f);
+            roundFastboot.SetColor(Android.Graphics.Color.DarkBlue);
+            fastbootButton.Background = roundFastboot;
+
+
+
+            recoveryButton.Click += (sender, e) =>
             {
                 statusText.Text = "Initiating recovery reboot...";
                 ExecuteRebootToRecovery();
@@ -44,7 +60,24 @@ namespace RecoveryTestApp
                 });
             }
         }
-    }
+        private void ExecuteRebootToFastboot()
+        {
+            try
+             {
+                var proc = Java.Lang.Runtime.GetRuntime().Exec("su -c reboot fastboot");
+                proc.WaitFor();
+            }
+            catch (Exception ex)
+            {
+                RunOnUiThread(() =>
+                {
+                    Toast.MakeText(this, $"Failed: {ex.Message}", ToastLength.Long).Show();
+                });
+            }
+        }
+
+
+    }  
 }
 	
 
